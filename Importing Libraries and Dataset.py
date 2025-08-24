c21ef53db58e83c07dc7eee4e3894018d74501cb
Mahendra Kapoor Kol <mahendra.2201242cs@iiitbh.ac.in>
2025-08-24 17:36:18 +0530

    Update Importing Libraries and Dataset.py

--- Importing Libraries and Dataset.py
+++ Importing Libraries and Dataset.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Load data
+data = pd.read_excel("HousePricePrediction.xlsx")
+
+# Show first 5 rows
+print(data.iloc[:5])
